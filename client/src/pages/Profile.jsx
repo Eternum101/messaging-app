f2cb286d9905ad5d2db0f03a0ce1822a942a6f64
Eternum101 <eternumhd111@gmail.com>
2024-01-04 19:27:24 +1000

    Modified Layout in Profile.jsx Added Fetch Functionality in Profile.jsx Modified Styling in Profile.css

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,28 +3,55 @@ import axios from "axios";
 import '../styles/Profile.css';
 import Sidebar from '../components/Sidebar';
 import Header from '../components/Header';
+import { FaPenToSquare } from "react-icons/fa6";
 
-function Profile() {
+function Profile({ loggedInUser }) {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (loggedInUser) {
+            axios.get(`/users/${loggedInUser.userId}`, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+            .then(response => {
+                setUser(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching user:', error);
+            });
+        }
+    }, [loggedInUser]);    
+
+    if (!user) {
+        return null; 
+    }
+
     return (
         <section className="profile-layout-container">
             <Header />
             <Sidebar />
             <div className="profile-container">
     <div className="profile-info">
+    <div className="edit-icon"><FaPenToSquare /></div>
         <div className="profile-avatar">
-            <img src="/images/default-profile-picture.jpg" alt="Profile" />
+            <img src={`/${user.image}`} alt={user.name} />
         </div>
     <div className='profile-titles'>
-        <h2>John Doe</h2>
-        <p>johndoe@gmail.com</p>
+        <h2>{user.firstName} {user.lastName}</h2>
+        <p>{user.email}</p>
     </div>
     </div>
     <div className="profile-about">
         <div className='profile-about-info'>
+        <div className="edit-icon"><FaPenToSquare /></div>
             <h3>About Me</h3>
             <p>Visual Designer with 7+ years of experience...</p>
         </div>
     <div className="profile-roles">
+    <div className="edit-icon"><FaPenToSquare /></div>
         <h3>Latest Roles</h3>
         <p>UI Designer at Specialized Bicycle</p>
         <p>Interaction Designer at Critical Mass / On-Site Apple</p>
